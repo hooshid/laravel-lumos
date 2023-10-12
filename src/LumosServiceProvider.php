e94c5a12b303ed1d6c3e879226141e3a180227c5
Alireza Kamali <arka1373@gmail.com>
2023-10-12 08:52:17 +0330

    Update LumosServiceProvider.php

--- src/LumosServiceProvider.php
+++ src/LumosServiceProvider.php
@@ -3,8 +3,13 @@
 namespace Hooshid\Lumos;
 
 use Hooshid\Lumos\View\Components\Alert;
+use Hooshid\Lumos\View\Components\Breadcrumb;
+use Hooshid\Lumos\View\Components\Btn;
+use Hooshid\Lumos\View\Components\Forms\Input;
+use Hooshid\Lumos\View\Components\Forms\Select;
+use Hooshid\Lumos\View\Components\Forms\Textarea;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
 
 class LumosServiceProvider extends ServiceProvider
 {
@@ -24,7 +29,32 @@ class LumosServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views/components', 'lumos');
-        Blade::componentNamespace('Hooshid\\Lumos\\View\\Components', 'lumos');
+        $this->bootResources();
+        $this->bootBladeComponents();
+    }
+
+    private function bootResources(): void
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'lumos-ui');
+    }
+
+    private function bootBladeComponents(): void
+    {
+        $this->callAfterResolving(BladeCompiler::class, function (BladeCompiler $blade) {
+            $namespace = 'lomus';
+
+            $components = [
+                'alert' => Alert::class,
+                'breadcrumb' => Breadcrumb::class,
+                'btn' => Btn::class,
+                'forms.input' => Input::class,
+                'forms.textarea' => Textarea::class,
+                'forms.select' => Select::class,
+            ];
+            
+            foreach ($components as $alias => $component) {
+                $blade->component($component, $namespace."::".$alias);
+            }
+        });
     }
 }
