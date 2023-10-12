ea22c7369ba88e2c282a8014e1719d79fa2ade5b
Alireza Kamali <arka1373@gmail.com>
2023-10-12 07:28:54 +0330

    Update Btn.php

--- src/View/Components/Btn.php
+++ src/View/Components/Btn.php
@@ -27,6 +27,6 @@ class Btn extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('lumos::btn');
+        return view('lumos::components.btn');
     }
 }
